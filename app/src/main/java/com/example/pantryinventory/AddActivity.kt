a476f1d6545373eb5d875d886259b8b4ead333eb
mitzyun <mqy2@georgetown.edu>
2022-02-13 23:02:16 -0500

    AddActivity currently crashing

---
+++ app/src/main/java/com/example/pantryinventory/AddActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pantryinventory
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class AddActivity: AppCompatActivity()  {
+    lateinit var itemName : EditText
+    lateinit var itemQuantity : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additem)
+        itemName = findViewById(R.id.itemName)
+        itemQuantity = findViewById(R.id.itemQuantity)
+
+    }//onCreate
+
+} // AddActivity
--- app/src/main/java/com/example/pantryinventory/MainActivity.kt
+++ app/src/main/java/com/example/pantryinventory/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.pantryinventory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.add, menu)
+        menuInflater.inflate(R.menu.additem_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }//onCreateOptionsMenu
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+
+        if (item.itemId == R.id.addItem) {
+            val intent = Intent(applicationContext, AddActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return false
+    }// onOptionsItemSelected
 
 } // MainActivity
