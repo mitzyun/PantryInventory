1260fc52f1c0c6f8b5a778574f4d331ef9dd2791
mitzyun <mqy2@georgetown.edu>
2022-02-13 21:40:29 -0500

    added menu

--- app/src/main/java/com/example/pantryinventory/MainActivity.kt
+++ app/src/main/java/com/example/pantryinventory/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.pantryinventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+    } //onCreate
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }//onCreateOptionsMenu
+
+} // MainActivity
